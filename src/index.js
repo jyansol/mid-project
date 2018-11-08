ba4c0f0663592582e5fde0ec35de8008d1e163f4
jyansol <hahn0406@gmail.com>
2018-11-08 12:55:04 +0900

    token가져오기

--- src/index.js
+++ src/index.js
@@ -33,7 +33,9 @@ const templates = {
 const rootEl = document.querySelector('.root');
 const logoEl = document.querySelector('.logo');
 const loginBtn = document.querySelector('.login-btn');
-const signUpnBtn = document.querySelector('.sign-up-btn');
+const signUpBtn = document.querySelector('.sign-up-btn');
+const oilsBtn = document.querySelector('.oils-btn');
+const logoutBtn = document.querySelector('.logout-btn');
 
 logoEl.addEventListener('click', (e) => {
   drawMainForm();
@@ -44,23 +46,29 @@ const drawMainForm = async () => {
   const frag = document.importNode(templates.homePage, true);
   const enterBtn = frag.querySelector('.enter-btn');
 
-  //enterBtn
+  //enterBtn을 누르면 postPage가 출력되는 이벤트
   enterBtn.addEventListener('click', async (e) => {
-    // e.preventDefault();
-    const frag = document.importNode(templates.postPage, true);
-    rootEl.textContent = '';
-    rootEl.appendChild(frag);
+    drawPostList();
   });
 
-  //loginBtn
+  //loginBtn을 누르면 로그인 폼이 나타나는 이벤트
   loginBtn.addEventListener('click', (e) => {
-    const frag = document.importNode(templates.loginForm, true);
-    rootEl.textContent = '';
-    rootEl.appendChild(frag);
+    if (localStorage.getItem('token')) {
+      alert('로그인됨');
+      drawMainForm();
+    } else {
+      drawLoginForm();
+    }
   });
 
-  //signUpBtn
-  signUpnBtn.addEventListener('click', (e) => {
+  //로그아웃 버튼을 누르면 token이 사라지게
+  logoutBtn.addEventListener('click', (e) => {
+    localStorage.removeItem('token');
+    drawMainForm();
+  });
+
+  //signUpBtn을 누르면 회원가입 폼이 나타나는 이벤트
+  signUpBtn.addEventListener('click', (e) => {
     const frag = document.importNode(templates.signForm, true);
     rootEl.textContent = '';
     rootEl.appendChild(frag);
@@ -70,15 +78,53 @@ const drawMainForm = async () => {
   rootEl.appendChild(frag);
 };
 
+// 로그인 폼이 나타나고, 로그인되는 함수
 const drawLoginForm = async () => {
   const frag = document.importNode(templates.loginForm, true);
-  const loginBtn = document.querySelector('.login-btn');
-  loginBtn.addEventListener('click', (e) => {
-    rootEl.textContent = '';
-    rootEl.appendChild(frag);
+  const formEl = frag.querySelector('.login-form');
+
+  // 전송버튼을 누르면
+  formEl.addEventListener('submit', async (e) => {
+    console.log('haha');
+    e.preventDefault();
+    const username = e.target.elements.username.value;
+    const password = e.target.elements.password.value;
+
+    const res = await api.post('/users/login', {
+      username,
+      password,
+    });
+
+    localStorage.setItem('token', res.data.token);
+    drawMainForm();
+  });
+
+  rootEl.textContent = '';
+  rootEl.appendChild(frag);
+};
+// drawLoginForm();
+
+// 상품 리스트 출력
+const drawPostList = async () => {
+  const frag = document.importNode(templates.postPage, true);
+  rootEl.textContent = '';
+  rootEl.appendChild(frag);
+  loginBtn.addEventListener('click', async (e) => {
+    if (localStorage.getItem('token')) {
+      document.
+    }
   });
 };
-drawLoginForm();
+
+// 로그인 함수
+
+// HomePage에서 버튼을 누르면 조건에 맞는 상품이 보여지는 함수
+const drawPostDetail = async () => {
+  const frag = document.importNode(templates.postPage, true);
+  const postList = frag.querySelector('.post-list');
+  const btn = frag.querySelector('.post-list');
+  oilsBtn.addEventListener('click', async (e) => {});
+};
 
 if (localStorage.getItem('token')) {
   drawPostList();
